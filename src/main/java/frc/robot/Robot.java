bd608c9970e4cbbf27a1ec10a8d04bfd59f63ab5
itsWihy <devwihy@gmail.com>
2026-01-18 18:51:59 +0200

    CRITICAL: DISABLEEE rev logging bro fawk rev

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import com.ctre.phoenix6.SignalLogger;
+import com.revrobotics.util.StatusLogger;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.lib.generic.hardware.HardwareManager;
@@ -16,8 +17,10 @@ public class Robot extends LoggedRobot {
 
     @Override
     public void robotInit() {
-        robotContainer = new RobotContainer();
         SignalLogger.enableAutoLogging(false);
+        StatusLogger.disableAutoLogging();
+
+        robotContainer = new RobotContainer();
         HardwareManager.initialize(this);
     }
 
