9c2314db2c27ce6bac68abdd6bedad4b109699bd
itsWihy <devwihy@gmail.com>
2026-01-18 18:51:07 +0200

    Feat: add support for CONSTRAINED PNP. Use algo to determine if robot is on the ground. Also add odometry pose to estimator.

--- src/main/java/frc/robot/poseestimation/PoseEstimator.java
+++ src/main/java/frc/robot/poseestimation/PoseEstimator.java
@@ -19,6 +19,7 @@ import org.littletonrobotics.junction.Logger;
 import java.util.Map;
 
 import static frc.robot.poseestimation.PoseEstimatorConstants.*;
+import static frc.robot.subsystems.swerve.SwerveConstants.GYRO;
 
 public class PoseEstimator {
     private final Field2d field = new Field2d();
@@ -39,6 +40,12 @@ public class PoseEstimator {
             VecBuilder.fill(VISION_STD_LINEAR, VISION_STD_LINEAR, VISION_STD_ANGULAR)
     );
 
+    private final SwerveDriveOdometry odometryEstimator = new SwerveDriveOdometry(
+            SwerveConstants.SWERVE_KINEMATICS,
+            Rotation2d.kZero,
+            positions
+    );
+
     private final Quest quest;
     private final Camera[] cameras;
 
@@ -51,6 +58,7 @@ public class PoseEstimator {
 
     public void resetPose(Pose2d pose) {
         poseEstimator.resetPose(pose);
+        odometryEstimator.resetPose(pose);
     }
 
     public Rotation2d getCurrentAngle() {
@@ -60,6 +68,11 @@ public class PoseEstimator {
     @AutoLogOutput(key = "PoseEstimator/CurrentPose")
     public Pose2d getCurrentPose() {
         return poseEstimator.getEstimatedPosition();
+    }
+
+    @AutoLogOutput(key = "PoseEstimator/OdometryPose")
+    public Pose2d getOdometryPose() {
+        return odometryEstimator.getPoseMeters();
     }
 
     public void periodic() {
@@ -109,6 +122,10 @@ public class PoseEstimator {
 
             poseEstimator.updateWithTime(
                     timestamp[i],
+                    gyroRotations[i],
+                    swerveWheelPositions[i]);
+
+            odometryEstimator.update(
                     gyroRotations[i],
                     swerveWheelPositions[i]);
         }
--- src/main/java/frc/robot/poseestimation/camera/EstimateData.java
+++ src/main/java/frc/robot/poseestimation/camera/EstimateData.java
@@ -7,17 +7,25 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 
+import static frc.lib.math.Optimizations.isRobotFlat;
 import static frc.robot.poseestimation.PoseEstimatorConstants.*;
+import static frc.robot.subsystems.swerve.SwerveConstants.GYRO;
 
-public record EstimateData(Pose3d pose, double timestamp, double distanceFromTag) {
+public record EstimateData(Pose3d pose, double timestamp, double distanceFromTag, CameraIO.PoseStrategy strategy) {
     public boolean isValid() {
-        final boolean rejectPose = Math.abs(pose.getZ()) > MAX_Z_ERROR
+        final boolean invalidPose = Math.abs(pose.getZ()) > MAX_Z_ERROR
                 || pose.getX() < 0.0
                 || pose.getX() > APRIL_TAG_FIELD_LAYOUT.getFieldLength()
                 || pose.getY() < 0.0
                 || pose.getY() > APRIL_TAG_FIELD_LAYOUT.getFieldWidth();
 
-        return !rejectPose;
+        if (invalidPose) return false;
+
+        if (strategy == CameraIO.PoseStrategy.CONSTRAINED_PNP) { //ROBOT SHOULD BE STRAIGHT.
+            return isRobotFlat();
+        }
+
+        return true;
     }
 
     public Matrix<N3, N1> getStandardDeviations() {
