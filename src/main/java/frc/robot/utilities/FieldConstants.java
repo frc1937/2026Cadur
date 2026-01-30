d108de3b14991b41ab436499b5e0d4311d79c5fb
itsWihy <105944317+itsWihy@users.noreply.github.com>
2026-01-30 17:24:27 +0200

    Fix: correct hub pose and clarify

--- src/main/java/frc/robot/utilities/FieldConstants.java
+++ src/main/java/frc/robot/utilities/FieldConstants.java
@@ -24,6 +24,8 @@ public class FieldConstants {
             FIELD_WIDTH = 8.07,
             FIELD_LENGTH = 16.54;
 
-    public static final FlippableTranslation3d HUB_POSITION = new FlippableTranslation3d(4.034536, 4.625594, 1.8288, true);
+    public static final FlippableTranslation3d HUB_TOP_POSITION
+            = new FlippableTranslation3d(4.604766, 4.0215, 1.8288, true);
+
     public static final FlippableTranslation2d TOWER_POSITION = new FlippableTranslation2d(3.730244, 1.016, true);
 }
