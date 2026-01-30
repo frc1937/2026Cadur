b82ca31cf71b67944664e97826d0a965e82a224a
itsWihy <105944317+itsWihy@users.noreply.github.com>
2026-01-30 20:06:33 +0200

    Style: improve documentation and readability in MotorConfiguration and TurretConstants

--- src/main/java/frc/robot/subsystems/shooter/turret/TurretConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/turret/TurretConstants.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.shooter.turret;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.lib.generic.GenericSubsystem;
 import frc.lib.generic.hardware.motor.*;
@@ -9,8 +8,10 @@ import frc.lib.generic.simulation.SimulationProperties;
 import frc.lib.generic.visualization.mechanisms.MechanismFactory;
 import frc.lib.generic.visualization.mechanisms.SingleJointedArmMechanism2d;
 
+import static edu.wpi.first.math.system.plant.DCMotor.getFalcon500;
 import static edu.wpi.first.units.Units.Second;
 import static edu.wpi.first.units.Units.Volts;
+import static frc.lib.generic.simulation.SimulationProperties.SimulationType.SIMPLE_MOTOR;
 import static frc.robot.utilities.PortsConstants.TurretPorts.TURRET_MOTOR_PORT;
 
 public class TurretConstants extends GenericSubsystem {
@@ -32,30 +33,30 @@ public class TurretConstants extends GenericSubsystem {
     }
 
     private static void configureTurretMotor() {
-        final MotorConfiguration turretMotorConfiguration = new MotorConfiguration();
+        final MotorConfiguration configuration = new MotorConfiguration();
 
-        turretMotorConfiguration.idleMode = MotorProperties.IdleMode.BRAKE;
+        configuration.idleMode = MotorProperties.IdleMode.BRAKE;
 
-        turretMotorConfiguration.slot = new MotorProperties.Slot(1, 0, 0, 0, 0, 0);
-        turretMotorConfiguration.profileMaxVelocity = 1.069;
-        turretMotorConfiguration.profileMaxAcceleration = 1.57; //TODO TUNE
+        configuration.slot = new MotorProperties.Slot(1, 0, 0, 0, 0, 0);//TODO TUNE
+        configuration.profileMaxVelocity = 1.069;//TODO TUNE
+        configuration.profileMaxAcceleration = 1.57; //TODO TUNE
 
-        turretMotorConfiguration.statorCurrentLimit = 40;
+        configuration.statorCurrentLimit = 40; //TODO TUNE
+        configuration.gearRatio = 100.0; //TODO TUNE
 
-        turretMotorConfiguration.simulationSlot = new MotorProperties.Slot(0, 0, 0, 11.22, 0, 0);
-        turretMotorConfiguration.simulationProperties = new SimulationProperties.Slot(
-                SimulationProperties.SimulationType.SIMPLE_MOTOR,
-                DCMotor.getFalcon500(1),
-                100,
-                0.045);
+        configuration.forwardSoftLimit = MAX_ANGLE.getRotations();
+        configuration.reverseSoftLimit = MIN_ANGLE.getRotations();
 
-        TURRET_MOTOR.configure(turretMotorConfiguration);
+        configuration.simulationSlot = new MotorProperties.Slot(0, 0, 0, 11.22, 0, 0);
+        configuration.simulationProperties = new SimulationProperties.Slot(SIMPLE_MOTOR, getFalcon500(1), 100, 0.045);
+
+        TURRET_MOTOR.configure(configuration);
 
         TURRET_MOTOR.setupSignalUpdates(MotorSignal.CURRENT);
         TURRET_MOTOR.setupSignalUpdates(MotorSignal.POSITION);
         TURRET_MOTOR.setupSignalUpdates(MotorSignal.VELOCITY);
         TURRET_MOTOR.setupSignalUpdates(MotorSignal.VOLTAGE);
-        TURRET_MOTOR.setupSignalUpdates(MotorSignal.ACCELERATION);
+        TURRET_MOTOR.setupSignalUpdates(MotorSignal.ACCELERATION); //TODO verify if needed
         TURRET_MOTOR.setupSignalUpdates(MotorSignal.CLOSED_LOOP_TARGET);
     }
 }
