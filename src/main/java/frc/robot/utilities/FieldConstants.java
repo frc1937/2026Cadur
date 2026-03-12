1eebe80e115e4b600b400ae045561ba860896114
itsWihy <devwihy@gmail.com>
2026-03-13 01:11:51 +0200

    Fix: further passing points so tis our alliance zone

--- src/main/java/frc/robot/utilities/FieldConstants.java
+++ src/main/java/frc/robot/utilities/FieldConstants.java
@@ -35,8 +35,8 @@ public class FieldConstants {
             = new FlippableTranslation3d(4.604766, HALF_FIELD_WIDTH, 1.8288, true);
 
     public static final Translation2d
-            LEFT_PASSING_POINT = new Translation2d(0.25 * HUB_TOP_POSITION.get().getX(), HUB_TOP_POSITION.get().getY() - 0.7),
-            RIGHT_PASSING_POINT = new Translation2d(0.25 * HUB_TOP_POSITION.get().getX(), HUB_TOP_POSITION.get().getY() + 0.7);
+            LEFT_PASSING_POINT = new Translation2d(0.25 * HUB_TOP_POSITION.get().getX(), HUB_TOP_POSITION.get().getY() - 2.2),
+            RIGHT_PASSING_POINT = new Translation2d(0.25 * HUB_TOP_POSITION.get().getX(), HUB_TOP_POSITION.get().getY() + 2.2);
 
     public static final FlippableTranslation2d TOWER_POSITION = new FlippableTranslation2d(3.730244, 1.016, true);
 
